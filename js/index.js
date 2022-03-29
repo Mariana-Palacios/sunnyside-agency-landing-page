c565bcfa93579fea70ddfc5b11c9c6dc1c60d9df
Mariana-Palacios <marianitaaax29@gmail.com>
2022-03-29 16:07:06 -0500

    title correction

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
-const para = document.getElementById('#btn__burger');
 const links = document.querySelector('.links');
-const nav__burger = document.querySelector('.nav__burger')
+const nav__burger = document.querySelector('.nav__burger');
+const top__text__title = document.querySelector('.top__text--title');
 
 function myFunc() {
     links.classList.toggle("is-active");
     nav__burger.classList.toggle("is-active");
-    console.log('funciono')
+    top__text__title.classList.toggle("is-active");
+    console.log('funciono');
 }
